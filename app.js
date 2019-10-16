0d63517f828cc51edb0c02dfa1d1c217a5f5d65b
captainandrey <43338827+captainandrey@users.noreply.github.com>
2019-10-16 12:25:41 +0200

    added routes

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+res.send('Hello World!!!!');
+
+});
+app.use(express.json());
+//routes
+app.use('/api/customers', require('./routes/api/customers'))
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
---
+++ routes/api/customers.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
+
+const customers = [
+    {
+        id: 1,
+        name: 'Homer Simpson',
+        gender: 'male'
+    },
+    {
+        id: 2,
+        name: 'Marge Simpson',
+        gender: 'female'
+    }
+
+];
+
+router.get('/', (req, res) => {
+    res.json(customers)
+});
+
+router.get('/:id', (req, res) => {
+    const customer = customers.find(c => c.id === parseInt(req.params.id));
+    if(customer){    
+    res.json(customer);
+    }
+    else{
+        res.status(404).json({msg: `Customer with id = ${req.params.id} not found`});
+    }
+});
+
+router.post('/', (req, res)=>{
+    const newCustomer = {
+        id: uuid.v4(),
+        name: req.body.name,
+        gender: req.body.gender
+    };
+    customers.push(newCustomer);
+
+    res.json(customers);
+
+
+});
+
+module.exports = router;
