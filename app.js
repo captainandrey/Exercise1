87a28edf7f961bf1b8add224788626cc47386036
captainandrey <43338827+captainandrey@users.noreply.github.com>
2019-10-17 12:33:10 +0200

    final version

--- app.js
+++ app.js
@@ -2,17 +2,6 @@ const express = require('express');
 
 const app = express();
 
-
-
-app.get('/', (req, res) => {
-res.send('Hello World!!!!');
-
-});
-
-
-
-
-
 app.use(express.json());
 //routes
 app.use('/api/customers', require('./routes/api/customers'))
--- routes/api/customers.js
+++ routes/api/customers.js
@@ -1,56 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const uuid = require('uuid');
 
 const {Datastore} = require('@google-cloud/datastore');
 const ds = new Datastore();
 const kind = 'Customer';
 
-/*const customers = [
-    {
-        id: 1,
-        name: 'Homer Simpson',
-        gender: 'male'
-    },
-    {
-        id: 2,
-        name: 'Marge Simpson',
-        gender: 'female'
-    }
-
-];*/
-
-
-/*
-async function listCustomers() {
-    const query = ds.createQuery([kind]).order('FirstName');
-  
-    const [customers] = await ds.runQuery(query);
-    console.log('Tasks:');
-    customers.forEach(customer => {
-      const key = customer[ds.KEY];
-      console.log(key.id, customer.FirstName);
-    });
-  }
-
- listCustomers();
-*/
-
-function fromDatastore(obj) {
+function mapper(obj) {
     obj.id = obj[ds.KEY].id;
     return obj;
   }
 
 router.get('/', async (req, res) => {
-    const query = ds.createQuery([kind]).order('FirstName');
+    const query = ds.createQuery([kind]);
     const [customers] = await ds.runQuery(query);
-    res.json(customers.map(fromDatastore));
+    res.json(customers.map(mapper));
 });
 
 router.get('/:id', (req, res) => {
-
     const key = ds.key([kind, parseInt(req.params.id, 10)]);
-    console.log(key);
     ds.get(key, (err, customer) => {
         if(!err && customer){
             customer.id = key.id;
@@ -60,17 +27,6 @@ router.get('/:id', (req, res) => {
             res.status(404).json({msg: `Customer with id = ${req.params.id} not found`});
         }
     });
-});
-
-router.post('/', (req, res)=>{
-    const newCustomer = {
-        id: uuid.v4(),
-        name: req.body.name,
-        gender: req.body.gender
-    };
-    customers.push(newCustomer);
-
-    res.json(customers);
 });
 
 module.exports = router;
